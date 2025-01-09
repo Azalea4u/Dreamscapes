d1df1996fbed67e41bf33935eb0b347cc6774b83
eneumont <esaintpierre@student.neumont.edu>
2025-01-09 16:19:55 -0700

    item button script added

---
+++ Assets/UI Stuff/Scripts/Scr_ItemBtn.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scr_ItemBtn : MonoBehaviour {
+    public Image itemImage;
+    public string itemName;
+    bool empty = true;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    public void onClick() { 
+    
+    }
+}
