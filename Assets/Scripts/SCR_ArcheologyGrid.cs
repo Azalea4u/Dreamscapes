af2e0a60dd13684ae8611cf964aeb947b5ae3219
flyguywannadie <60896082+flyguywannadie@users.noreply.github.com>
2025-01-09 13:49:21 -0700

    Archeology Minigame started
    Made an empty scene and a script for the grid.

---
+++ Assets/Scripts/SCR_ArcheologyGrid.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_ArcheologyGrid : MonoBehaviour
+{
+    [SerializeField] private Vector2 gridSize = Vector2.one;
+    [SerializeField] private List<GameObject> testing = new List<GameObject>();
+    [SerializeField] private GameObject tilePrefab;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
