416e67c4bbfd79d63dabcc065931924db48ea6ee
flyguywannadie <60896082+flyguywannadie@users.noreply.github.com>
2025-01-13 16:08:00 -0700

    Dragon Groups
    Dragons can now be a part of groups, so I can uniformly randomize and organize large sets of them

--- Assets/Scripts/SCR_FindDragon_Dragon.cs
+++ Assets/Scripts/SCR_FindDragon_Dragon.cs
@@ -4,47 +4,60 @@ public class SCR_FindDragon_Dragon : MonoBehaviour
 {
     public Vector2 speed = Vector2.one;
     public edgeType edgeInteraction = edgeType.BOUNCE;
+	[SerializeField] private SpriteRenderer sprite;
 
     // helps with defining how the dragon should act when contacting an edge
     public enum edgeType
     {
-        BOUNCE,
-        SCROLL
+        BOUNCE = 0,
+        SCROLL = 1
     }
 
 	private void Start()
 	{
-		speed = new Vector2(Random.Range(-2.0f, 2.0f), Random.Range(-2.0f, 2.0f));
+		//speed = new Vector2(Random.Range(-2.0f, 2.0f), Random.Range(-2.0f, 2.0f));
+		//edgeInteraction = (edgeType)Random.Range(0, 2);
 	}
 
 	void Update()
     {
         transform.Translate(new Vector3(speed.x, speed.y) * Time.deltaTime);
 
-        if (Mathf.Abs(transform.position.x) > SCR_FindDragon_Manager.instance.gameBounds.x)
-        {
-			switch (edgeInteraction)
-			{
-                case edgeType.BOUNCE:
-                    speed.x *= -1;
-                    break;
-                case edgeType.SCROLL:
-                    transform.position.Scale(new Vector3(-1, 1, 0));
-                    break;
-			}
-		}
-
-		if (Mathf.Abs(transform.position.y) > SCR_FindDragon_Manager.instance.gameBounds.y)
+		switch (edgeInteraction)
 		{
-			switch (edgeInteraction)
-			{
-				case edgeType.BOUNCE:
+			case edgeType.BOUNCE:
+				if (Mathf.Abs(transform.position.x) > SCR_FindDragon_Manager.instance.gameBounds.x)
+				{
+					speed.x *= -1;
+				}
+				if (Mathf.Abs(transform.position.y) > SCR_FindDragon_Manager.instance.gameBounds.y)
+				{
 					speed.y *= -1;
-					break;
-				case edgeType.SCROLL:
-					transform.position.Scale(new Vector3(1, -1, 0));
-					break;
-			}
+				}
+				break;
+			case edgeType.SCROLL:
+				if (transform.position.x > SCR_FindDragon_Manager.instance.gameBounds.x)
+				{
+					transform.position -= new Vector3(SCR_FindDragon_Manager.instance.gameBounds.x * 2.0f, 0, 0);
+				}
+				if (transform.position.x < -SCR_FindDragon_Manager.instance.gameBounds.x)
+				{
+					transform.position += new Vector3(SCR_FindDragon_Manager.instance.gameBounds.x * 2.0f, 0, 0);
+				}
+				if (transform.position.y > SCR_FindDragon_Manager.instance.gameBounds.y)
+				{
+					transform.position -= new Vector3(0, SCR_FindDragon_Manager.instance.gameBounds.y * 2.0f, 0);
+				}
+				if (transform.position.y < -SCR_FindDragon_Manager.instance.gameBounds.y)
+				{
+					transform.position += new Vector3(0, SCR_FindDragon_Manager.instance.gameBounds.y * 2.0f, 0);
+				}
+				break;
 		}
+	}
+
+	public void SetSprite(Sprite sprite)
+	{
+		this.sprite.sprite = sprite;
 	}
 }
--- Assets/Scripts/SCR_FindDragon_Manager.cs
+++ Assets/Scripts/SCR_FindDragon_Manager.cs
@@ -1,26 +1,70 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SCR_FindDragon_Manager : MonoBehaviour
 {
     public static SCR_FindDragon_Manager instance { get; private set; }
 
-    [SerializeField] private GameObject[] dragons;
+    [SerializeField] private SCR_FindDragon_Dragon[] dragons;
+    [SerializeField] private Sprite[] dragonSprites;
     [SerializeField] public Vector2 gameBounds;
+    //[SerializeField] private int difficulty;
+
+    [SerializeField] private List<dragonGroup> dragonGroups = new List<dragonGroup>();
+
+    // helper struct for organizing groups of dragons
+    public struct dragonGroup
+    {
+        public Sprite sprite;
+        public Vector2 speed;
+        public bool copySpeed;
+        public SCR_FindDragon_Dragon.edgeType edgeType;
+    }
 
 	private void Start()
 	{
 		instance = this;
-		SetupDragons();
+        createDragonGroups();
+		setupDragons();
 	}
 
-	private void SetupDragons()
+    private void createDragonGroups()
+    {
+        int start = Random.Range(0,dragonSprites.Length);
+        for (int i = 0; i < 3; i++)
+        {
+			dragonGroup newgroup = new dragonGroup();
+            newgroup.sprite = dragonSprites[(start + i)%dragonSprites.Length];
+			newgroup.copySpeed = (Random.Range(0, 2)==0);
+            if (newgroup.copySpeed)
+            {
+                newgroup.speed = new Vector2((Random.Range(0, 2) * 2 - 1) * Random.Range(1.0f, 2.0f), (Random.Range(0, 2) * 2 - 1) * Random.Range(1.0f, 2.0f));
+            } else
+            {
+                newgroup.speed = new Vector2(Random.Range(1.0f, 2.0f), Random.Range(1.0f, 2.0f));
+			}
+            newgroup.edgeType = (SCR_FindDragon_Dragon.edgeType)Random.Range(0, 2);
+            dragonGroups.Add(newgroup);
+		}
+    }
+
+	private void setupDragons()
     {
         foreach (var dragon in dragons)
         {
             dragon.transform.position = new Vector3(Random.Range(-gameBounds.x, gameBounds.x), Random.Range(-gameBounds.y, gameBounds.x), 0);
-            
+
+            dragonGroup usedgroup = dragonGroups[Random.Range(0, dragonGroups.Count)];
+            if (usedgroup.copySpeed)
+            {
+				dragon.speed = usedgroup.speed;
+			} else
+            {
+                Vector2 s = usedgroup.speed;
+                dragon.speed = new Vector2(Random.Range(-s.x, s.x), Random.Range(-s.y, s.y));
+			}
+            dragon.SetSprite(usedgroup.sprite);
+            dragon.edgeInteraction = usedgroup.edgeType;
         }
-        
-        
     }
 }
