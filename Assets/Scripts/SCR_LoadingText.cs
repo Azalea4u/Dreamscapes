49cb2b729af8ab577c597b8992d65e1978584c33
MakenzieBuchenroth-Neumont <mbuchenroth@student.neumont.edu>
2025-01-21 14:47:10 -0700

    Finished loading screen (... type one at a time and credits are displayed)

---
+++ Assets/Scripts/SCR_LoadingText.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+public class SCR_LoadingText : MonoBehaviour {
+	[SerializeField] TextMeshProUGUI loadingText;
+	private float timer = 1f;
+
+	async void Update() {
+		timer =- Time.deltaTime;
+		if (loadingText.text == "Loading.") {
+		await Task.Delay(1000);
+			loadingText.text = "Loading..";
+		}
+		else if (loadingText.text == "Loading..") {
+		await Task.Delay(1000);
+			loadingText.text = "Loading...";
+		}
+		else if (loadingText.text == "Loading...") {
+		await Task.Delay(1000);
+			loadingText.text = "Loading.";
+		}
+		if (timer < 0f) timer = 1f;
+	}
+}
