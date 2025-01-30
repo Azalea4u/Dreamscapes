1ec954761b396d132b421bf2c13b9da5a7d853fa
eneumont <esaintpierre@student.neumont.edu>
2025-01-30 14:13:19 -0700

    a

--- Assets/UI Stuff/Scripts/Scr_ScreenManager.cs
+++ Assets/UI Stuff/Scripts/Scr_ScreenManager.cs
@@ -2,13 +2,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Scr_ScreenManager : Singleton<Scr_ScreenManager> {
-    [SerializeField] GameObject titleS;
-    [SerializeField] GameObject playS;
-    [SerializeField] GameObject pauseS;
-    [SerializeField] GameObject menuS;
-
-	bool pause = false;
-    bool menu = false;
+	//bool pause = false;
+ //   bool menu = false;
 
 	void Start() {
         
@@ -19,35 +14,35 @@ public class Scr_ScreenManager : Singleton<Scr_ScreenManager> {
     }
 
     public void PlayClick() {
-        playS.SetActive(true);
-        titleS.SetActive(false);
+        //playS.SetActive(true);
+        //titleS.SetActive(false);
     }
 
     public void QuitClick() {
-        pauseS.SetActive(false);
-        titleS.SetActive(true);
+        //pauseS.SetActive(false);
+        //titleS.SetActive(true);
     }
 
     public void PauseClick() {
-		pause = !pause;
-        if (pause) {
-            Time.timeScale = 0;
-            pauseS.SetActive(true);
-            playS.SetActive(false);
-        } else { 
-            Time.timeScale = 1;
-			pauseS.SetActive(false);
-			playS.SetActive(true);
-		}
+		//pause = !pause;
+  //      if (pause) {
+  //          Time.timeScale = 0;
+  //          pauseS.SetActive(true);
+  //          playS.SetActive(false);
+  //      } else { 
+  //          Time.timeScale = 1;
+		//	pauseS.SetActive(false);
+		//	playS.SetActive(true);
+		//}
 	}
 
     public void MenuClick() {
-        menu = !menu;
-        if (menu) {
-            menuS.SetActive(true);
-            return;
-        }
-        menuS.SetActive(false);
+        //menu = !menu;
+        //if (menu) {
+        //    menuS.SetActive(true);
+        //    return;
+        //}
+        //menuS.SetActive(false);
 
     }
 
@@ -62,7 +57,12 @@ public class Scr_ScreenManager : Singleton<Scr_ScreenManager> {
 	}
 
     public void DragonClick() {
-        SceneManager.LoadScene("");
+        SceneManager.LoadScene("SCN_FindDragonLuigi");
+		Destroy(gameObject);
+	}
+
+    public void OctopusClick() {
+		SceneManager.LoadScene("Octopus");
 		Destroy(gameObject);
 	}
 }
