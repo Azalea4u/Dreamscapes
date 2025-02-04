f6b3027d970dd54f880074c725773fb79b003847
flyguywannadie <60896082+flyguywannadie@users.noreply.github.com>
2025-02-04 16:49:06 -0700

    Making obstacles; Separated ship and Manager

---
+++ Assets/Scripts/SpaceGame/SCR_SpaceGame_Ship.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+
+public class SCR_SpaceGame_Ship : MonoBehaviour
+{
+	[SerializeField] private SpriteRenderer visuals;
+	[SerializeField] private Sprite[] damageStates;
+	[SerializeField] private int health = 3;
+	private float shipPosition;
+	[SerializeField] private float shipBounds;
+	[SerializeField] private float shipAscentSpeed;
+	[SerializeField] private float shipMovementSpeed;
+	private float desiredAscentSpeed;
+	private float usedAscentSpeed = 0;
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		desiredAscentSpeed = shipAscentSpeed;
+		visuals.sprite = damageStates[health];
+	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+		usedAscentSpeed = Mathf.Lerp(usedAscentSpeed, desiredAscentSpeed, 3.0f * Time.fixedDeltaTime);
+
+		transform.position = new Vector3(shipPosition, transform.position.y + (Time.fixedDeltaTime * usedAscentSpeed), 0.0f);
+	}
+
+	public void MoveLeft()
+	{
+		shipPosition -= shipMovementSpeed * Time.fixedDeltaTime;
+		shipPosition = Mathf.Clamp(shipPosition, -shipBounds, shipBounds);
+	}
+
+	public void MoveRight()
+	{
+		shipPosition += shipMovementSpeed * Time.fixedDeltaTime;
+		shipPosition = Mathf.Clamp(shipPosition, -shipBounds, shipBounds);
+	}
+
+	public void DamageShip()
+	{
+		health -= 1;
+		if (health < 0)
+		{
+			// test stuff for now
+			Debug.Log("Ship Has Died");
+			health = 0;
+		}
+		visuals.sprite = damageStates[health];
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		// Debug.Log("Boombaby");
+		SCR_SpaceshipDamager obstacle = collision.GetComponent<SCR_SpaceshipDamager>();
+
+		if (obstacle != null)
+		{
+			desiredAscentSpeed *= 1.0f - obstacle.slowDownAscent;
+			shipMovementSpeed *= 1.0f - obstacle.slowDownMovement;
+			if (!obstacle.beenHit)
+			{
+				usedAscentSpeed -= obstacle.bounce;
+				if (obstacle.doesDamage)
+				{
+					DamageShip();
+				}
+			}
+			obstacle.HitShip();
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		//Debug.Log("Goodbye");
+		SCR_SpaceshipDamager obstacle = collision.GetComponent<SCR_SpaceshipDamager>();
+
+		if (obstacle != null)
+		{
+			desiredAscentSpeed /= 1.0f - obstacle.slowDownAscent;
+			shipMovementSpeed /= 1.0f - obstacle.slowDownMovement;
+		}
+	}
+}
