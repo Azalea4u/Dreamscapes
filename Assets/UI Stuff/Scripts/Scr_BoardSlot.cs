9cbfaf5590436b7f8212add16cc43fc0f3a55eb9
eneumont <esaintpierre@student.neumont.edu>
2025-02-07 17:50:12 -0700

    leader board mostly done
    leader board mostly done just need to do some slight json tweaking

--- Assets/UI Stuff/Scripts/Scr_BoardSlot.cs
+++ Assets/UI Stuff/Scripts/Scr_BoardSlot.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Scr_BoardSlot : MonoBehaviour {
-    [SerializeField] Image slotImg;
-    [SerializeField] TextMeshProUGUI scoreTxt;
+    public Image slotImg;
+    public TextMeshProUGUI scoreTxt;
 
     public void changeSlot(Image img, int score) {
 		slotImg = img;
