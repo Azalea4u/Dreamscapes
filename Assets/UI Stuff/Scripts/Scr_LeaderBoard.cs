d1f1f7d86316237159f360e08321595d33d49f56
eneumont <esaintpierre@student.neumont.edu>
2025-02-13 14:21:07 -0700

    leaderboard and find again... =(
    added and tweaked leaderboard, dragon game slightly easier

--- Assets/UI Stuff/Scripts/Scr_LeaderBoard.cs
+++ Assets/UI Stuff/Scripts/Scr_LeaderBoard.cs
@@ -3,19 +3,19 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
 using UnityEditor;
+using System.Collections;
 
 public class Scr_LeaderBoard : MonoBehaviour {
     [SerializeField] GameObject createPanel;
     [SerializeField] GameObject continueBtn;
+    [SerializeField] GameObject againBtn;
     [SerializeField] GameObject loserpanel;
     [SerializeField] Scr_BoardSlot[] slots;
     [SerializeField] Sprite[] sprites;
 
     int newScore;
-    float time = 3;
-    bool countDown = false;
 
-    //good luck =)
+    //good luck >=)
     [SerializeField] TextAsset info;
     [System.Serializable]
     class slotSlot {
@@ -53,67 +53,106 @@ public class Scr_LeaderBoard : MonoBehaviour {
             }
         }
 
-        //these are just tests do not uncomment them
+        //these are just tests do not uncomment them >=(
         //loser();
         //highScore();
     }
 
-    void Update() {
-        if (countDown) { 
-            time -= Time.deltaTime;
-            if (time <= 0) {
-                loserpanel.SetActive(false);
-                continueBtn.SetActive(true);
-                countDown = false;
-                time = 3;
-            }
-        }
-    }
+	IEnumerator loserCoroutine() {
+		yield return new WaitForSeconds(3);
+		loserpanel.SetActive(false);
+		continueBtn.SetActive(true);
+		againBtn.SetActive(true);
+	}
+
+	void saveStuff() {
+		for (int i = 0; i < slots.Length; i++) {
+			saveSlots.slot[i].s = int.Parse(slots[i].scoreTxt.text);
+
+			switch (slots[i].slotImg.sprite.name) {
+				case "finn_0":
+					saveSlots.slot[i].i = "finn";
+					break;
+				case "jake_0":
+					saveSlots.slot[i].i = "jake";
+					break;
+				case "bmo_0":
+					saveSlots.slot[i].i = "bmo";
+					break;
+				case "bubblegum_0":
+					saveSlots.slot[i].i = "bubblegum";
+					break;
+				case "question_0":
+					saveSlots.slot[i].i = "???";
+					break;
+			}
+		}
+
+		File.WriteAllText(Application.dataPath + "/JSON-Files(Txt)/" + info.name + ".txt", JsonUtility.ToJson(saveSlots));
+		AssetDatabase.Refresh();
+	}
 
     public void continueClick() {
-        for (int i = 0; i < slots.Length; i++) {
-            saveSlots.slot[i].s = int.Parse(slots[i].scoreTxt.text);
-
-            switch (slots[i].slotImg.sprite.name) {
-                case "finn_0":
-                    saveSlots.slot[i].i = "finn";
-                    break;
-                case "jake_0":
-                    saveSlots.slot[i].i = "jake";
-                    break;
-                case "bmo_0":
-                    saveSlots.slot[i].i = "bmo";
-                    break;
-                case "bubblegum_0":
-                    saveSlots.slot[i].i = "bubblegum";
-                    break;
-                case "question_0":
-                    saveSlots.slot[i].i = "???";
-                    break;
-            }
-        }
-
-        File.WriteAllText(Application.dataPath + "/JSON-Files(Txt)/" + info.name + ".txt", JsonUtility.ToJson(saveSlots));
-		AssetDatabase.Refresh();
+        saveStuff();
 
 		SCR_Loader.Load(SCR_Loader.scenes.SCN_MainMenu);
 	}
+
+    public void againClick() {
+        saveStuff();
+
+        switch (info.name) {
+            case "Archeology":
+				SCR_Loader.Load(SCR_Loader.scenes.SCN_ArcheologyMinigame);
+				break;
+            case "Find":
+				SCR_Loader.Load(SCR_Loader.scenes.SCN_FindDragonLuigi);
+				break;
+            case "Octopus":
+				SCR_Loader.Load(SCR_Loader.scenes.SCN_OctopusShooter);
+				break;
+            case "Rocket":
+				SCR_Loader.Load(SCR_Loader.scenes.SCN_SpaceshipScene);
+				break;
+        }
+    }
 
     public void createClick(Image img) {
         createSlot(img, newScore);
 
         createPanel.SetActive(false);
         continueBtn.SetActive(true);
-    }
+		againBtn.SetActive(true);
+	}
 
     void createSlot(Image img, int score, int index = 0) {
-        //index; doesn't work so I just do this
+        //index; doesn't work so I just do this, theres probably an easier way...
 		for (index = index; index < slots.Length; index++) {
             Image tempImg;
             int tempScore;
+
 			if (int.Parse(slots[index].scoreTxt.text) < score) {
                 tempImg = slots[index].slotImg;
-                tempScore = int.Parse(slots[index].scoreTxt.text);
+
+				switch (slots[index].slotImg.sprite.name) {
+					case "finn_0":
+						tempImg.sprite = sprites[0];
+						break;
+					case "jake_0":
+						tempImg.sprite = sprites[1];
+						break;
+					case "bmo_0":
+						tempImg.sprite = sprites[2];
+						break;
+					case "bubblegum_0":
+						tempImg.sprite = sprites[3];
+						break;
+					case "question_0":
+						tempImg.sprite = sprites[4];
+						break;
+				}
+
+				tempScore = int.Parse(slots[index].scoreTxt.text);
                 slots[index].changeSlot(img, score);
                 createSlot(tempImg, tempScore, index + 1);
                 return;
@@ -123,12 +162,14 @@ public class Scr_LeaderBoard : MonoBehaviour {
 
     void highScore() {
         createPanel.SetActive(true);
+        continueBtn.SetActive(false);
+        againBtn.SetActive(false);
     }
 
     void loser() {
         loserpanel.SetActive(true);
         loserpanel.GetComponentInChildren<TextMeshProUGUI>().text = "Your score: " + newScore;
-        countDown = true;
+        StartCoroutine(loserCoroutine());
     }
 
     public void endGame(int score) {
