6c5437d8af6df852ff72f9fc87e725ba68cc58bf
Azalea4u <dnguyen@student.neumont.edu>
2025-02-19 15:35:42 -0700

    Added some SFX to Spaceship

--- Assets/Scripts/SpaceGame/SCR_SpaceGame_Manager.cs
+++ Assets/Scripts/SpaceGame/SCR_SpaceGame_Manager.cs
@@ -39,7 +39,6 @@ public class SCR_SpaceGame_Manager : MonoBehaviour
 
     [Header("UI")]
     [SerializeField] private TextMeshProUGUI distance_TXT;
-	[SerializeField] private AudioSource hitCloud_SFX;
 
     private void Start()
 	{
