f36aa51ca9c52ddc3ab4a33b2940cc7d0e1da554
Azalea4u <dnguyen@student.neumont.edu>
2025-02-24 16:50:21 -0700

    Fixed Particle Spawn

--- Assets/UI Stuff/Scripts/Scr_Bullet.cs
+++ Assets/UI Stuff/Scripts/Scr_Bullet.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Scr_Bullet : MonoBehaviour {
@@ -5,6 +6,8 @@ public class Scr_Bullet : MonoBehaviour {
 	Rigidbody2D rb;
     [SerializeField] ParticleSystem ps;
     float time;
+
+    private string layer;
 
     void Start() {
         transform.position = FindAnyObjectByType<Scr_OctoPlayer>().transform.position;
@@ -15,25 +18,48 @@ public class Scr_Bullet : MonoBehaviour {
     void Update() {
 		if (transform.position.y >= 6.0f)
 		{
+            //StartCoroutine(WaitForParticles());
             Destroy(gameObject);
-            Instantiate(ps, visuals.position, Quaternion.identity);
+            //Instantiate(ps, visuals.position, Quaternion.identity);
         }
-		visuals.Rotate(Vector3.forward, Time.deltaTime * -100);
+        visuals.Rotate(Vector3.forward, Time.deltaTime * -100);
 	}
 
 	private void OnTriggerEnter2D(Collider2D other) 
     {
-        if (other.GetComponent<Scr_Bomb>())
-        {
-            Destroy(gameObject);
-            //Instantiate(ps, other.transform);
-        }
-        if (other.GetComponents<Scr_OctoEnemy>().Length > 0) {
 
-            other.GetComponent<Scr_OctoEnemy>().damage(1);
-            
-            Destroy(gameObject);
+        if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
             Instantiate(ps, other.transform);
+            ps.Play();
+
+            if (other.gameObject.tag == "Enemy")
+            {
+                other.GetComponent<Scr_OctoEnemy>().damage(1);
+            }
+
+            StartCoroutine(WaitForParticles());
+            Destroy(gameObject);
         }
+
+        //ps.Play();
+        //if (other.GetComponent<Scr_Bomb>())
+        //{
+        //    //StartCoroutine(WaitForParticles());
+        //    Destroy(gameObject);
+        //}
+        //if (other.GetComponents<Scr_OctoEnemy>().Length > 0) {
+        //
+        //    //StartCoroutine(WaitForParticles());
+        //    other.GetComponent<Scr_OctoEnemy>().damage(1);
+        //
+        //    Destroy(gameObject);
+        //}
 	}
+
+    private IEnumerator WaitForParticles()
+    {
+        //ps.Play();
+        yield return new WaitForSeconds(0.2f);
+    }
 }
