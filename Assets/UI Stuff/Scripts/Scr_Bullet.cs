44d336a748c137f7032a397c88b20998fa32990e
Azalea4u <118029861+Azalea4u@users.noreply.github.com>
2025-02-25 11:47:04 -0700

    Revert "Revert "Update Dian's Branch""

--- Assets/UI Stuff/Scripts/Scr_Bullet.cs
+++ Assets/UI Stuff/Scripts/Scr_Bullet.cs
@@ -1,8 +1,13 @@
+using System.Collections;
 using UnityEngine;
 
 public class Scr_Bullet : MonoBehaviour {
 	[SerializeField] Transform visuals;
 	Rigidbody2D rb;
+    [SerializeField] ParticleSystem ps;
+    float time;
+
+    private string layer;
 
     void Start() {
         transform.position = FindAnyObjectByType<Scr_OctoPlayer>().transform.position;
@@ -13,20 +18,48 @@ public class Scr_Bullet : MonoBehaviour {
     void Update() {
 		if (transform.position.y >= 6.0f)
 		{
-			Destroy(gameObject);
-		}
-		visuals.Rotate(Vector3.forward, Time.deltaTime * -100);
+            //StartCoroutine(WaitForParticles());
+            Destroy(gameObject);
+            //Instantiate(ps, visuals.position, Quaternion.identity);
+        }
+        visuals.Rotate(Vector3.forward, Time.deltaTime * -100);
 	}
 
-	private void OnTriggerEnter2D(Collider2D other) {
+	private void OnTriggerEnter2D(Collider2D other) 
+    {
 
-        if (other.GetComponent<Scr_Bomb>())
+        if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
+            Instantiate(ps, other.transform);
+            ps.Play();
+
+            if (other.gameObject.tag == "Enemy")
+            {
+                other.GetComponent<Scr_OctoEnemy>().damage(1);
+            }
+
+            StartCoroutine(WaitForParticles());
             Destroy(gameObject);
         }
-        if (other.GetComponents<Scr_OctoEnemy>().Length > 0) {
-            other.GetComponent<Scr_OctoEnemy>().damage(1);
-            Destroy(gameObject);
-        }
+
+        //ps.Play();
+        //if (other.GetComponent<Scr_Bomb>())
+        //{
+        //    //StartCoroutine(WaitForParticles());
+        //    Destroy(gameObject);
+        //}
+        //if (other.GetComponents<Scr_OctoEnemy>().Length > 0) {
+        //
+        //    //StartCoroutine(WaitForParticles());
+        //    other.GetComponent<Scr_OctoEnemy>().damage(1);
+        //
+        //    Destroy(gameObject);
+        //}
 	}
+
+    private IEnumerator WaitForParticles()
+    {
+        //ps.Play();
+        yield return new WaitForSeconds(0.2f);
+    }
 }
